b708f4eef439acc8971bd234976dc6b83ac7acb8
Allen Hsu <allenwhsu@gmail.com>
2023-01-28 17:28:38 -0500

    add cursor follower

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const urlEntries = "https://api.github.com/repos/twbs/bootstrap/releases/latest"
-
 $.ajax ({
 	url: urlEntries,
 	success: function (result) {
-		console.log(result);
 		$(".bv").text(result.name.slice(1));
 	}
 })
 
 const urlEntries2 = "https://api.github.com/repos/jquery/jquery/releases/latest"
-
 $.ajax ({
 	url: urlEntries2,
 	success: function (result) {
-		console.log(result);
 		$(".jv").text(result.tag_name);
 	}
 })
 
-const urlEntries2 = "https://api.github.com/repos/sindresorhus/modern-normalize/releases/latest"
-
+const urlEntries3 = "https://api.github.com/repos/sindresorhus/modern-normalize/releases/latest"
 $.ajax ({
 	url: urlEntries3,
 	success: function (result) {
-		console.log(result);
 		$(".nv").text(result.tag_name);
+	}
+})
+
+const urlEntries4 = "https://api.github.com/repos/twbs/bootstrap/releases/latest"
+$.ajax ({
+	url: urlEntries4,
+	success: function (result) {
+		var bduDist = result.assets[0].browser_download_url;
+		$(".bdu-dist").attr("href", bduDist);
+	}
+})
+
+const urlEntries5 = "https://api.github.com/repos/twbs/bootstrap/releases/latest"
+$.ajax ({
+	url: urlEntries5,
+	success: function (result) {
+		var bduSrc = "https://github.com/twbs/bootstrap/archive/" + result.tag_name + ".zip";
+		$(".bdu-src").attr("href", bduSrc);
 	}
 })
